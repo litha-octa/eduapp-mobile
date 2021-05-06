c82c7257839198b68c7408fa02c4d36d643da9fe
litha <lithaoctadelistia@gmail.com>
2021-05-07 01:57:15 +0700

    login

---
+++ src/screens/Login.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity,
+} from 'react-native';
+import {LoginStyle} from '../components/auth/LoginStyle';
+
+const Login = ({ navigation, route }) => {
+return(
+        <View style={LoginStyle.body}>
+            <Text style={LoginStyle.title}>Login</Text>
+            <Text style={LoginStyle.descInput}>Username or Email</Text>
+            <TextInput style={LoginStyle.inputLogin}/>
+            <Text style={LoginStyle.descInput}>Password</Text>
+            <TextInput style={LoginStyle.inputLogin} />
+        <TouchableOpacity
+            style={LoginStyle.buttonLogin}>
+            <Text style={LoginStyle.labelButtonLogin}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+            style={LoginStyle.buttonLoginWithGoogle}>
+            <Text style={LoginStyle.labelButtonLoginWithGoogle}>
+                Login with Google
+            </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+            onPress={() => {
+                navigation.navigate('Register');
+            }}>
+            <Text>Dont have account ? Register here</Text>
+        </TouchableOpacity>
+        </View>
+    );
+};
+export default Login;
